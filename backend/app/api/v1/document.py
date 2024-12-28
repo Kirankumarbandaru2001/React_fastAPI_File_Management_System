0b5cd1443a1faa63d20acc5a9d60d1719f94c149
Kirankumarbandaru2001 <Kirankumarbandaru2001@gmail.com>
2024-12-28 16:43:47 +0530

    Basic Structure

--- backend/app/api/v1/document.py
+++ backend/app/api/v1/document.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, File, UploadFile
 from app.services.file_storage import save_file
 
 router = APIRouter()
-
+# File upload
 @router.post("/upload")
 async def upload_document(file: UploadFile = File(...)):
     await save_file(file)
